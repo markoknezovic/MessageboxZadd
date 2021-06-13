bcae4ce66d06009c4043d086c92cc51abc1c5ae0
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-06-13 22:37:05 +0200

    Add files via upload

---
+++ MessageBox/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MessageBoks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonOtvori = new System.Windows.Forms.Button();
+            this.textBoxRezultat = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonOtvori
+            // 
+            this.buttonOtvori.Location = new System.Drawing.Point(345, 99);
+            this.buttonOtvori.Name = "buttonOtvori";
+            this.buttonOtvori.Size = new System.Drawing.Size(75, 23);
+            this.buttonOtvori.TabIndex = 0;
+            this.buttonOtvori.Text = "prikazi";
+            this.buttonOtvori.UseVisualStyleBackColor = true;
+            this.buttonOtvori.Click += new System.EventHandler(this.btnPrikazi_Click);
+            // 
+            // textBoxRezultat
+            // 
+            this.textBoxRezultat.Location = new System.Drawing.Point(312, 128);
+            this.textBoxRezultat.Multiline = true;
+            this.textBoxRezultat.Name = "textBoxRezultat";
+            this.textBoxRezultat.Size = new System.Drawing.Size(135, 79);
+            this.textBoxRezultat.TabIndex = 1;
+            this.textBoxRezultat.TextChanged += new System.EventHandler(this.txtKutijaZaUpis_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxRezultat);
+            this.Controls.Add(this.buttonOtvori);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOtvori;
+        private System.Windows.Forms.TextBox textBoxRezultat;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ MessageBox/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtKutijaZaUpis_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrikazi_Click(object sender, EventArgs e)
+        {
+            string poruka = "Odaberite Yes / No / Cancel";
+
+            string naslov = "Naslovna linija Dijaloga za poruke";
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, MessageBoxButtons.YesNoCancel);
+            
+            switch (rez)
+
+            {
+
+                case DialogResult.Yes:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme YES";
+
+                    break;
+
+                case DialogResult.No:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme NO";
+
+                    break;
+
+                case DialogResult.Cancel:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme Cancel";
+
+                    break;
+
+            }
+
+        }
+    }
+}
